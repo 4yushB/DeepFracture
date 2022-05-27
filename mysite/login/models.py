cf87b27fa0db7ca098bc4c2bda10dfae5d388ded
Ayush <kushal.a.bilash@student.uts.edu.au>
2022-05-27 14:45:56 +1000

    Site Uploaded
    Base website files uploaded

---
+++ mysite/login/models.py
@@ -0,0 +1,35 @@
+"""Login Page."""
+from django.db import models
+
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+
+from streams import blocks
+
+# Create your models here.
+
+class LoginPage(Page):
+    """login page class"""
+    template = "flex/flex_page2.html"
+
+    content = StreamField(
+        [
+            ("title_and_text", blocks.TitleAndTextBlock(classname='text_and_title')),
+            ("full_rich_text", blocks.RichtextBlock()),
+            ("simple_rich_text", blocks.SimpleRichtextBlock()),
+            ("cards", blocks.CardBlock()),
+        ],
+        null=True,
+        blank=True
+    )
+
+
+    content_panels = Page.content_panels + [
+
+        StreamFieldPanel("content"),
+    ]
+
+    class Meta:
+        verbose_name = "Login Page"
+        verbose_name_plural = "Login Pages"
